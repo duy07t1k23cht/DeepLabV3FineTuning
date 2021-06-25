04118d57a044c2ba5d53950fabf847d2227f1dc0
duy07t1k23cht <bbbshurikengerbb@gmail.com>
2021-06-25 18:29:56 +0700

    Edit save model

--- sources/train.py
+++ sources/train.py
@@ -116,10 +116,10 @@ def train_model(model, num_classes, dataloaders, criterion, optimizer, device, d
                 val_acc_history.append(epoch_acc)
 
             # Save current model every 25 epochs
-            if 0 == epoch%25:
-                current_model_path = os.path.join(dest_dir, f"checkpoint_{epoch:04}_DeepLabV3_Skydiver.pth")
-                print(f"Save current model : {current_model_path}")
-                torch.save(model.state_dict(), current_model_path)
+            # if 0 == epoch%25:
+            current_model_path = os.path.join(dest_dir, f"checkpoint_{epoch:04}_DeepLabV3_Skydiver.pth")
+            print(f"Save current model : {current_model_path}")
+            torch.save(model.state_dict(), current_model_path)
 
         print()
 
--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python sources/main_training.py ./custom_dataset ./training_output --num_classes 2 --epochs 100 --batch_size 16 --keep_feature_extract
+python sources/main_training.py ./sample_dataset ./training_output --num_classes 5 --epochs 100 --batch_size 16 --keep_feature_extract
