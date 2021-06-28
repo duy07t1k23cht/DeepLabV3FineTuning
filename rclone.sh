fad8aeb4a2b504f72c9fe488b4bcfdd787a88025
duy07t1k23cht <bbbshurikengerbbb@gmail.com>
2021-06-28 10:18:12 +0000

    train resize 244

--- rclone.sh
+++ rclone.sh
@@ -1,5 +1,5 @@
 while true
 do 
-    rclone copy ./training_output/ mason:/DATN_Project/logs_DeepLabV3/logs_28062021 -vv
-    sleep 600
+    rclone copy ./training_output/ mason:/DATN_Project/logs_DeepLabV3/logs_28062021_resize_224 -vv
+    sleep 300
 done
--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python3 sources/main_training.py ./sky_seg_2806 ./training_output --num_classes 2 --epochs 5000 --batch_size 32 --keep_feature_extract
+python3 sources/main_training.py ./sky_seg_2806 ./training_output --num_classes 2 --epochs 5000 --batch_size 64 --keep_feature_extract
